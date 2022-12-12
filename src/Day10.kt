3f4c76a4f20fb6d36d5eaf00a55319c7d30aff94
arkady <arksap2002@gmail.com>
2022-12-12 16:38:21 +0100

    add solution 10

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
         var result = 0
@@ -25,11 +27,44 @@ fun main() {
         return result
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>) {
+        var i = 0
+        var x = 1
+        var result = ""
+        for (line in input) {
+            if (line == "noop") {
+                i++
+                result += if (abs(x - (result.length % 40)) <= 1) {
+                    "#"
+                } else {
+                    "."
+                }
+            } else {
+                val value = line.split(" ")[1].toInt()
+                i++
+                result += if (abs(x - (result.length % 40)) <= 1) {
+                    "#"
+                } else {
+                    "."
+                }
+                i++
+                result += if (abs(x - (result.length % 40)) <= 1) {
+                    "#"
+                } else {
+                    "."
+                }
+                x += value
+            }
+        }
+        for (j in result.indices) {
+            print(result[j])
+            if ((j + 1) % 40 == 0) {
+                println()
+            }
+        }
     }
 
     val input = readInput("Day10")
     part1(input).println()
-    part2(input).println()
+    part2(input)
 }
